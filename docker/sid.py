a8ea802ee0a2a4451909f07087cd9f8b6343c2df
Siddartha <siddu@Siddarthas-Air.lan>
2023-08-19 13:44:38 -0500

    added new file

---
+++ docker/sid.py
@@ -0,0 +1,10 @@
+# This program calculates the square of a number
+
+# Input
+number = float(input("Enter a number: "))
+
+# Calculate square
+square = number ** 2
+
+# Output
+print("The square of", number, "is", square)
